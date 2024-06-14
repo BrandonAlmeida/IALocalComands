0cc836be2c26ef139a0621cf432b39ec18d743cc
BrandonAlmeida <bran.almeida.s@protonmail.com>
2024-06-14 00:54:50 -0300

    Separação das funções em outro arquivo. Criação de função para retorno de voz.

--- ChatGpt/main.py
+++ ChatGpt/main.py
@@ -1,209 +1,52 @@
+#!/bin/python3
+from openai import OpenAI
 import subprocess
 import os
-from time import sleep
-from openai import OpenAI
 from rich.markdown import Markdown
 from rich.console import Console
 from colorama import Fore, Style
-import threading
 import sys
-import signal
-import socket
+import pygame
+from utils import get_assistant, create_thread, obter_input, add_message_to_thread, create_and_poll_run, list_thread_messages, transform_input_to_list, execute_command, upload_files, search_files, tts
+
 wrkpath = os.getcwd()
 
 
-print(os.getenv('USER'))
-def get_user():
-    return f"CHATGPT {os.getenv('USER')}"
-
-def get_host():
-    return socket.gethostname()
-
-def get_current_dir():
-    return os.getcwd()
-
-def get_git_branch():
-    try:
-        branch = subprocess.check_output(
-            ["git", "branch", "--show-current"],
-            stderr=subprocess.STDOUT
-        ).strip().decode('utf-8')
-        if branch:
-            return f"‹{branch}› "
-        return ""
-    except subprocess.CalledProcessError:
-        return ""
-
-def get_return_code():
-    return_code = os.getenv('?', 0)
-    return f"{return_code} ↵" if return_code != 0 else ""
-
-def get_venv():
-    venv = os.getenv("VIRTUAL_ENV")
-    if venv:
-        return f"‹{os.path.basename(venv)}› "
-    return ""
-
-def get_prompt():
-    user_host = f"\033[1m\033[92m{get_user()}👽:\033[91m{get_host()}\033[0m"
-    current_dir = f"📁:\033[1m\033[93m{get_current_dir()} \033[0m"
-    vcs_branch = get_git_branch()
-    venv_prompt = get_venv()
-    return_code = get_return_code()
-    user_symbol = "#" if os.geteuid() == 0 else "$"
-    
-    prompt = f"╭─{user_host}{current_dir}{venv_prompt}{vcs_branch}\n╰─\033[1m{user_symbol}\033[0m "
-    rprompt = f"\033[1m{return_code}\033[0m"
-    
-    return prompt, rprompt
-
-#Coleta ID do Assitente e APIKEY
-try:
-    with open(f"{wrkpath}/ChatGpt/config", "r", encoding="utf-8") as file:
-        lines = file.readlines()
-        apikey = lines[0].strip()
-        assistant_id = lines[1].strip()
-except Exception as Err:
-    print("Arquivo de configuração não encontrado")
-    apikey = str(input("Informe sua APIKEY: "))
-    assistant_id = str(input("Informe o ID do ASSISTENTE: "))
-
-# Configuração do cliente OpenAI
-client = OpenAI(api_key=apikey)
-
-def execute_command(command):
-    # Inicia o processo com Popen
-    if "cd " in command[0:3]:
-        dpath = command[3:]
-        os.chdir(dpath)
-        cmdoutdec = subprocess.check_output("pwd", shell=True).decode()
-
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    output_lines = []
-
-    def read_output(stream, prefix=''):
-        while True:
-            line = stream.readline()
-            if not line:
-                break
-            print(f"{prefix}{line.strip()}")
-            output_lines.append(f"{prefix}{line.strip()}")
-
-    stdout_thread = threading.Thread(target=read_output, args=(process.stdout,))
-    stderr_thread = threading.Thread(target=read_output, args=(process.stderr, 'Erro: '))
-
-    stdout_thread.start()
-    stderr_thread.start()
-
-    def signal_handler(sig, frame):
-        print('Interrompido! Encerrando o comando...')
-        process.terminate()
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    stdout_thread.join()
-    stderr_thread.join()
-
-    # Espera o processo terminar e retorna o código de saída
-    process.wait()
-    rc = process.returncode
-
-    complete_output = "\n".join(output_lines)
-    return complete_output, rc
-
-#Função para criar lista de comandos
-def transform_input_to_list(input_str):
-    # Divida a string de entrada por vírgulas
-    command_list = input_str.split(',')
-    # Remova espaços em branco no início e no fim de cada comando
-    command_list = [cmd.strip() for cmd in command_list]
-    return command_list
-
-# Função para buscar o assistente
-def get_assistant(client, assistant_id):
-    return client.beta.assistants.retrieve(assistant_id=assistant_id)
-
-# Função para criar uma thread
-def create_thread(client):
-    return client.beta.threads.create()
-
-# Função para adicionar mensagem à thread
-def add_message_to_thread(client, thread_id, content):
-    return client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=content
-    )
-
-# Função para criar e monitorar execução da thread
-def create_and_poll_run(client, thread_id, assistant_id):
-    run = client.beta.threads.runs.create_and_poll(
-        thread_id=thread_id,
-        assistant_id=assistant_id,
-        instructions=""
-    )
-
-    while run.status != "completed":
-        print(run.status)
-        sleep(2)
-        run = client.beta.threads.runs.retrieve(run.id)  # Atualiza o status da run
-
-    return run
-
-# Função para listar mensagens da thread
-def list_thread_messages(client, thread_id):
-    return client.beta.threads.messages.list(thread_id=thread_id)
-
-# Função para fazer upload e monitoramento de arquivos
-def upload_files(client, file_paths):
-    file_streams = [open(path, "rb") for path in file_paths]
-
-    vector_store = client.beta.vector_stores.create(name="Uploaded Files")
-
-    file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-        vector_store_id=vector_store.id, files=file_streams
-    )
-
-    print(file_batch.status)
-    print(file_batch.file_counts)
-
-    return vector_store.id
-
-#Função para consultar arquivos
-def search_files(client, vector_store_id, query):
-    search_result = client.beta.vector_stores.search(
-        vector_store_id=vector_store_id,
-        query=query
-    )
-    return search_result
-
-#Estilização do input
-def obter_input():
-    """Obtém o input do usuário com destaque em amarelo."""
-    print(f"{Fore.BLUE}{Style.BRIGHT}---{Style.RESET_ALL}")
-    prompt, rprompt = get_prompt()
-    print(prompt, end="")
-    try:
-        input_lines = []
-        while True:
-            line = input()
-            input_lines.append(line)
-    except EOFError:
-        input_text = '\n'.join(input_lines)
-
-    #input_text = input(f"{Fore.GREEN}{Style.BRIGHT}>>> ")
-    print(f"{Fore.BLUE}{Style.BRIGHT}---{Style.RESET_ALL}")
-    return input_text
-
 # Main function
 def main():
+    
+    audio = 0
+    #Coleta ID do Assitente e APIKEY
+    try:
+        with open(f"{wrkpath}/ChatGpt/config", "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            apikey = lines[0].strip()
+            assistant_id = lines[1].strip()
+    except Exception as err:
+        print(f"Arquivo de configuração não encontrado, erro: {err}")
+        apikey = str(input("Informe sua APIKEY: "))
+        assistant_id = str(input("Informe o ID do ASSISTENTE: "))
+
+    # Configuração do cliente OpenAI
+    client = OpenAI(api_key=apikey)
+    
     assistant = get_assistant(client, assistant_id)
     thread = create_thread(client)
     vector_store_id = None  # Inicialize como None
 
     while True:
+
         quest = obter_input()
+        if "audio()" in quest[0:8]:
+            if audio == 0:
+                audio = 1
+                pygame.mixer.init()
+                print("Retorno de voz ligado.")
+            else:
+                audio = 0
+                print("Retorno de voz desligado.")
+            continue
+        
         if "exit()" in quest[0:7]:
             sys.exit()
         if "cmd:" in quest[0:4]:
@@ -233,7 +76,9 @@ def main():
                     mdformat = Markdown(chat_return)
                     console = Console()
                     console.print(mdformat)
-                    
+                    if audio == 1:
+                        tts(client, chat_return)
+                        
             except subprocess.CalledProcessError as error:
                 err = f"Falha na execução do comando, erro: {error}"
                 add_message_to_thread(client, thread.id, err)
@@ -282,6 +127,8 @@ def main():
             mdformat = Markdown(chat_return)
             console = Console()
             console.print(mdformat)
+            if audio == 1:
+                tts(client, chat_return)
 
 if __name__ == "__main__":
     main()
---
+++ ChatGpt/utils.py
@@ -0,0 +1,200 @@
+import signal
+import pygame
+import subprocess
+import socket
+import threading
+from pathlib import Path
+from colorama import Fore, Style
+from time import sleep
+import os
+
+def get_user():
+    return f"CHATGPT {os.getenv('USER')}"
+
+def get_host():
+    return socket.gethostname()
+
+def get_current_dir():
+    return os.getcwd()
+
+def get_git_branch():
+    try:
+        branch = subprocess.check_output(
+            ["git", "branch", "--show-current"],
+            stderr=subprocess.STDOUT
+        ).strip().decode('utf-8')
+        if branch:
+            return f"‹{branch}› "
+        return ""
+    except subprocess.CalledProcessError:
+        return ""
+
+def get_return_code():
+    return_code = os.getenv('?', 0)
+    return f"{return_code} ↵" if return_code != 0 else ""
+
+def get_venv():
+    venv = os.getenv("VIRTUAL_ENV")
+    if venv:
+        return f"‹{os.path.basename(venv)}› "
+    return ""
+
+def get_prompt():
+    user_host = f"\033[1m\033[92m{get_user()}👽:\033[91m{get_host()}\033[0m"
+    current_dir = f"📁:\033[1m\033[93m{get_current_dir()} \033[0m"
+    vcs_branch = get_git_branch()
+    venv_prompt = get_venv()
+    return_code = get_return_code()
+    user_symbol = "#" if os.geteuid() == 0 else "$"
+    
+    prompt = f"╭─{user_host}{current_dir}{venv_prompt}{vcs_branch}\n╰─\033[1m{user_symbol}\033[0m "
+    rprompt = f"\033[1m{return_code}\033[0m"
+    
+    return prompt, rprompt
+
+#Função Text to Speech
+def tts(client, text):
+    speech_file_path = Path(__file__).parent
+    with client.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="echo",
+        input=text,
+    ) as response:
+        response.stream_to_file(f"{speech_file_path}/speech.mp3")
+        
+    pygame.mixer.music.load(f"{speech_file_path}/speech.mp3")
+    pygame.mixer.music.play(loops=0, start=0.0)
+    while pygame.mixer.music.get_busy():
+        pygame.time.delay(100)
+        
+    os.remove(f"{speech_file_path}/speech.mp3")    
+
+
+#Função para executar comandos no sistema operacional
+def execute_command(command):
+    # Inicia o processo com Popen
+    if "cd ~" in command[0:4]:
+        home_path = os.getenv('HOME')
+        command = command.replace("~", home_path)
+    if "cd " in command[0:3]:
+        dpath = command[3:]
+        os.chdir(dpath)
+        cmdoutdec = subprocess.check_output("pwd", shell=True).decode()
+    else:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    output_lines = []
+
+    def read_output(stream, prefix=''):
+        while True:
+            line = stream.readline()
+            if not line:
+                break
+            print(f"{prefix}{line.strip()}")
+            output_lines.append(f"{prefix}{line.strip()}")
+
+    stdout_thread = threading.Thread(target=read_output, args=(process.stdout,))
+    stderr_thread = threading.Thread(target=read_output, args=(process.stderr, 'Erro: '))
+
+    stdout_thread.start()
+    stderr_thread.start()
+
+    def signal_handler(sig, frame):
+        print('Interrompido! Encerrando o comando...')
+        process.terminate()
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    stdout_thread.join()
+    stderr_thread.join()
+
+    # Espera o processo terminar e retorna o código de saída
+    process.wait()
+    rc = process.returncode
+
+    complete_output = "\n".join(output_lines)
+    return complete_output, rc
+
+#Função para criar lista de comandos
+def transform_input_to_list(input_str):
+    # Divida a string de entrada por vírgulas
+    command_list = input_str.split(',')
+    # Remova espaços em branco no início e no fim de cada comando
+    command_list = [cmd.strip() for cmd in command_list]
+    return command_list
+
+# Função para buscar o assistente
+def get_assistant(client, assistant_id):
+    return client.beta.assistants.retrieve(assistant_id=assistant_id)
+
+# Função para criar uma thread
+def create_thread(client):
+    return client.beta.threads.create()
+
+# Função para adicionar mensagem à thread
+def add_message_to_thread(client, thread_id, content):
+    return client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=content
+    )
+
+# Função para criar e monitorar execução da thread
+def create_and_poll_run(client, thread_id, assistant_id):
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+        instructions=""
+    )
+
+    while run.status != "completed":
+        print(run.status)
+        sleep(2)
+        run = client.beta.threads.runs.retrieve(run.id)  # Atualiza o status da run
+
+    return run
+
+# Função para listar mensagens da thread
+def list_thread_messages(client, thread_id):
+    return client.beta.threads.messages.list(thread_id=thread_id)
+
+# Função para fazer upload e monitoramento de arquivos
+def upload_files(client, file_paths):
+    file_streams = [open(path, "rb") for path in file_paths]
+
+    vector_store = client.beta.vector_stores.create(name="Uploaded Files")
+
+    file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+        vector_store_id=vector_store.id, files=file_streams
+    )
+
+    print(file_batch.status)
+    print(file_batch.file_counts)
+
+    return vector_store.id
+
+#Função para consultar arquivos
+def search_files(client, vector_store_id, query):
+    search_result = client.beta.vector_stores.search(
+        vector_store_id=vector_store_id,
+        query=query
+    )
+    return search_result
+
+#Estilização do input
+def obter_input():
+    """Obtém o input do usuário com destaque em amarelo."""
+    print(f"{Fore.BLUE}{Style.BRIGHT}---{Style.RESET_ALL}")
+    prompt, rprompt = get_prompt()
+    print(prompt, end="")
+    try:
+        input_lines = []
+        while True:
+            line = input()
+            input_lines.append(line)
+    except EOFError:
+        input_text = '\n'.join(input_lines)
+
+    #input_text = input(f"{Fore.GREEN}{Style.BRIGHT}>>> ")
+    print(f"{Fore.BLUE}{Style.BRIGHT}---{Style.RESET_ALL}")
+    return input_text
